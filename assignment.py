8e339b93d421e0c048d67ef57d9be288e4119795
amandalxllll <xliu7@babson.edu>
2023-03-24 14:49:01 -0400

    finished

--- assignment.py
+++ assignment.py
@@ -78,14 +78,14 @@ stopwords = list(stopwords.keys())
 
 # a. Count the Total words
 def total_words(book):
-    """Returns the total of the frequencies in a histogram."""
+    """Returns the total of the frequencies in a book."""
     return sum(book.values())
 
 
 # b. count the most common words
 def most_common(book, excluding_stopwords=False):
     """Makes a list of word-freq pairs in descending order of frequency.
-    hist: map from word to frequency
+    book: map from word to frequency
     returns: list of (frequency, word) pairs
     """
 
@@ -116,6 +116,9 @@ def top_10_words(book):
 
 
 def top_overlappingwords(book_1, book_2):
+    """
+    This returnd the overlapping words appeared in top_10 words from two books.
+    """
     b1 = top_10_words(book_1)
     b2 = top_10_words(book_2)
     lt = []
@@ -134,8 +137,7 @@ def top_overlappingwords(book_1, book_2):
 
 def top_nonoverlappingwords(book_1, book_2):
     """
-    Returns a dictionary with all keys that appear in book1 but not book2.
-    d1, d2: dictionaries
+    This returnd the nonoverlapping words appeared in top_10 words from two books.
     """
     b1 = top_10_words(book_1)
     b2 = top_10_words(book_2)
@@ -152,14 +154,18 @@ def top_nonoverlappingwords(book_1, book_2):
             list_nonoverlapping.append(i)
     return list_nonoverlapping
 
+
 def count_marr_words(book):
+    """
+    This returnd the words contaning words with marr: married. marry. marriage. marrying from two books.
+    """
     marr_count = 0
     book_string = str(book)
     final_book = book_string.lower()
     for word in final_book.split():
-        if 'marr' in word:
+        if "marr" in word:
             marr_count += 1
-        elif 'mar' in word and len(word) > 4:
+        elif "mar" in word and len(word) > 4:
             # Check for variations like "marriage", "married", etc.
             marr_count += 1
     return marr_count
@@ -179,10 +185,6 @@ def text_similarity(book1, book2):
     return result
 
 
-# Part 5: Text Visualization
-
-
-
 def main():
     je = process_file("janeeyre.txt", skip_header=False)
     lw = process_file("littlewomen.txt", skip_header=False)
@@ -194,17 +196,21 @@ def main():
     print(f"The top 10 words in Jane Eyre are:{top_10_words(je)}")
     print(f"The top 10 words in Little Women are:{top_10_words(lw)}")
     print(
-       f"The non-overlapping words in the top 10 common words in two books are:{top_nonoverlappingwords(je, lw)}"
+        f"The non-overlapping words in the top 10 common words in two books are:{top_nonoverlappingwords(je, lw)}"
     )
     print(
-       f"The overlapping words in the top 10 common words in two books are:{top_overlappingwords(je, lw)}"
+        f"The overlapping words in the top 10 common words in two books are:{top_overlappingwords(je, lw)}"
     )
-    print(f'The occurence of words related to marriage in Jane Eyre are {count_marr_words(je)}')
-    print(f'The occurence of words related to marriage in the Little Women are {count_marr_words(lw)}')
+    print(
+        f"The occurence of words related to marriage in Jane Eyre are {count_marr_words(je)}"
+    )
+    print(
+        f"The occurence of words related to marriage in the Little Women are {count_marr_words(lw)}"
+    )
     print(f"The sentiment analysis result of Jane Eyre is{sentiment(je_nltk)}")
     print(f"The sentiment analysis result of the Little Women is{sentiment(lw_nltk)}")
     print(f"The text similarity based in the fuzz ratio is {text_similarity(je,lw)}%")
-    
+
 
 if __name__ == "__main__":
     main()
